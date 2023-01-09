dffd24b939810c6407a6ddf84fce3940ff0d2152
Klemen Slavič <krof.drakula@gmail.com>
2023-01-09 16:17:45 +0100

    WIP: extract JSDocs

--- packages/storybook/src/convert_type.test.ts
+++ packages/storybook/src/convert_type.test.ts
@@ -6,13 +6,12 @@ import convertType from "./convert_type";
 const getExportedType = (
   sourceFile: SourceFile,
   exportedName: string
-): Type | undefined => {
+): Type | void => {
   for (const [name, declarations] of sourceFile.getExportedDeclarations()) {
     if (name == exportedName) {
       return declarations[0].getType();
     }
   }
-  return undefined;
 };
 
 test("should return empty ArgTypes when given non-iterable, non-object values", () => {
@@ -23,10 +22,7 @@ test("should return empty ArgTypes when given non-iterable, non-object values", 
       export const myProps = false;
     `
   );
-  const argTypes = convertType(
-    getExportedType(sourceFile, "myProps")!,
-    project.getTypeChecker()
-  );
+  const argTypes = convertType(getExportedType(sourceFile, "myProps")!);
   expect(argTypes).toEqual({});
 });
 
@@ -41,14 +37,11 @@ test("should return an object description when props are iterable objects", () =
         age: 3,
         array: ['three'],
         object: { one: 'two' },
-        slap: () => null
+        slap: () => 4
       };
     `
   );
-  const argTypes = convertType(
-    getExportedType(sourceFile, "myProps")!,
-    project.getTypeChecker()
-  );
+  const argTypes = convertType(getExportedType(sourceFile, "myProps")!);
   expect(argTypes).toEqual({
     truthy: { type: "boolean" },
     name: { type: "string" },
@@ -56,5 +49,29 @@ test("should return an object description when props are iterable objects", () =
     array: {},
     object: {},
     slap: { type: "function" },
+  });
+});
+
+test("it should provide parameter descriptions from JSDoc", () => {
+  const project = new Project({ tsConfigFilePath: "./tsconfig.json" });
+  const sourceFile = project.createSourceFile(
+    "a.ts",
+    dedent`
+      export const myProps = {
+        /**
+         * Provides the name of the person. It should work
+         * with multiline descriptions.
+         */
+        name: 'hello'
+      };
+    `
+  );
+  const argTypes = convertType(getExportedType(sourceFile, "myProps")!);
+  expect(argTypes).toEqual({
+    name: {
+      type: "string",
+      description:
+        "Provides the name of the person. It should work\nwith multiline descriptions.",
+    },
   });
 });
--- packages/storybook/src/convert_type.ts
+++ packages/storybook/src/convert_type.ts
@@ -1,29 +1,44 @@
-import { SyntaxKind, Type, TypeChecker } from "ts-morph";
-import { ArgTypes } from "@storybook/types";
+import { Node, SyntaxKind, Type } from "ts-morph";
+import { ArgTypes, InputType } from "@storybook/types";
 
-const convertType = (type: Type, typeChecker: TypeChecker): ArgTypes => {
+const getDescription = (node: Node): string | void => {
+  if (node.isKind(SyntaxKind.PropertyAssignment)) {
+    return node.compilerNode.jsDoc?.length > 0
+      ? node.compilerNode.jsDoc.map((c) => c.comment).join("\n")
+      : undefined;
+  }
+};
+
+const getStorybookType = (node: Node): InputType["type"] | void => {
+  if (node.isKind(SyntaxKind.PropertyAssignment)) {
+    const initializer = node.getInitializer();
+    if (!initializer) return;
+    const value = initializer.getType().getBaseTypeOfLiteralType();
+    if (!value) return;
+    if (value.isBoolean()) {
+      return "boolean";
+    } else if (value.isNumber()) {
+      return "number";
+    } else if (value.isString()) {
+      return "string";
+    } else if (value.getCallSignatures().length > 0) {
+      return "function";
+    }
+  }
+};
+
+const convertType = (type: Type): ArgTypes => {
   const result: ArgTypes = {};
   if (type.isObject()) {
     for (const property of type.getProperties()) {
       const name = property.getName();
-      const [decl] = property.getDeclarations();
-      const assignment = decl.asKind(SyntaxKind.PropertyAssignment);
-      if (!assignment?.getInitializer()) continue;
-      const value = typeChecker
-        .getTypeAtLocation(assignment.getInitializer()!)
-        .getBaseTypeOfLiteralType();
-      if (!value) continue;
-      if (value.isBoolean()) {
-        result[name] = { type: "boolean" };
-      } else if (value.isNumber()) {
-        result[name] = { type: "number" };
-      } else if (value.isString()) {
-        result[name] = { type: "string" };
-      } else if (value.getCallSignatures().length > 0) {
-        result[name] = { type: "function" };
-      } else {
-        result[name] = {};
-      }
+      const [decl] = property.getDeclarations() ?? [];
+      const propAssignment = decl.asKind(SyntaxKind.PropertyAssignment);
+      if (!propAssignment) continue;
+      const valueType = getStorybookType(propAssignment);
+      result[name] = valueType ? { type: valueType } : {};
+      const description = getDescription(propAssignment);
+      if (description) result[name].description = description;
     }
   }
   return result;
